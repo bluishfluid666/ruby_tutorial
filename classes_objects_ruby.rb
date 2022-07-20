b248a658c9ad3b9df2156ba2f04e6735a82393c1
bluishfluid666 <nguyenmanhkhang0805@gmail.com>
2022-07-20 17:17:24 +0700

    Learn Ruby classes and objects

---
+++ classes_objects_ruby.rb
@@ -0,0 +1,27 @@
+class Customer
+  @@total_no_of_customers = 0
+  def initialize(id, name="", addr)
+    @id = id
+    @name = name
+    @addr = addr
+    @@total_no_of_customers += 1;
+  end
+  def display_details
+    puts "Customer ID: #@id"
+    puts "Customer name: #@name"
+    puts "Customer address: #@addr"
+  end
+  def total_no_of_customers
+    puts "Total number of customers in the system: #@@total_no_of_customers"
+  end
+  def self.total_cust
+    @@total_no_of_customers
+  end
+end
+c1 = Customer.new(1, "John Homelander", "69 Great Ave")
+c2 = Customer.new(2, "Annie January", "Iowa")
+c3 = Customer.new(3, "Hugh Campbell", "NYC")
+c4 = Customer.new(4, "Mother's Milk", "New Jersey")
+c4.display_details
+# c4.total_no_of_customers
+puts Customer.total_cust
